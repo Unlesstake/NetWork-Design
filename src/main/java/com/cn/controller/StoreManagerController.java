e0edde310280799e012a43af2ba820ae6d55c01e
songyang <747566474@qq.com>
2020-04-06 17:27:56 +0800

    :tada: feat:新增电商数据编辑、删除功能

--- src/main/java/com/cn/controller/StoreManagerController.java
+++ src/main/java/com/cn/controller/StoreManagerController.java
@@ -4,11 +4,11 @@ package com.cn.controller;
 import com.cn.dao.StoreDao;
 import com.cn.entity.Admin;
 import com.cn.entity.Store;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
@@ -58,7 +58,7 @@ public class StoreManagerController {
         return "user/StoreManagerDetails";
     }
 
-    @RequestMapping("/StoreManager/Update/{id}")
+    @RequestMapping("/StoreManager/Update/{id}")                        //前往编辑页面
     public String UpdateIndex(@PathVariable("id") int id, Model model){
         Store store = storedao.FindById(id);
         model.addAttribute("store",store);
@@ -73,5 +73,58 @@ public class StoreManagerController {
             }
         }
         return "user/StoreManagerUpdate";
+    }
+
+    @PostMapping("/StoreManagerUpdate")                 //更新电商数据
+    @ResponseBody
+    public String StoreManagerUpdate(@RequestBody Store NewStore){
+        int flag = storedao.UpdateStore(NewStore);
+        if(flag>0){
+            JSONObject object = new JSONObject();
+            object.put("code", 200);
+            object.put("desc", "修改成功！");
+            object.put("data", NewStore);
+            return object.toString();
+        }
+        JSONObject object = new JSONObject();
+        object.put("code",400);
+        object.put("desc","修改失败，请检查数据后重试！");
+        return  object.toString();
+    }
+
+    @PostMapping("/StoreManagerDelete")
+    @ResponseBody
+    public String StoreManagerDelete(@RequestBody Store DeStore){
+        int flag = storedao.DelStore(DeStore.getStore_name());
+        if(flag>0){
+            JSONObject object = new JSONObject();
+            object.put("code", 200);
+            object.put("desc", "删除电商数据成功！");
+            return object.toString();
+        }
+        JSONObject object = new JSONObject();
+        object.put("code", 400);
+        object.put("desc", "删除失败，请稍后重试！");
+        return object.toString();
+    }
+
+    @RequestMapping("/StoreManager/Add")                 /*前往新增用户页面*/
+    public String GoAdd(Model model){
+        Cookie[] cookies = request.getCookies();
+        HttpSession session = request.getSession();
+        for (Cookie cookie : cookies) {
+            if ("token_admin".equals(cookie.getName())) {
+                Admin admin = (Admin) session.getAttribute(cookie.getValue());
+                model.addAttribute("UserName",admin.getUsername());
+                break;
+            }
+        }
+        return "user/StoreManagerAdd";
+    }
+
+    @PostMapping("StoreManagerAdd")
+    @ResponseBody
+    public String StoreManagerAdd(@RequestBody Store AddStore){
+        return "user";
     }
 }
--- src/main/java/com/cn/dao/StoreDao.java
+++ src/main/java/com/cn/dao/StoreDao.java
@@ -9,4 +9,6 @@ import org.apache.ibatis.annotations.Param;
 public interface StoreDao {
     List<Store> FindAll();      //查询所有电商数据
     Store FindById(Integer id);  //通过id查找对用电商数据
+    int UpdateStore(Store store); //更新电商数据
+    int DelStore(String store_name);  //删除电商数据
 }
